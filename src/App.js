68814b9870b990042c265fd5037fcecc5eb4fb6f
Sanjar Saparov <117997805+sanzarr0704@users.noreply.github.com>
2023-02-01 09:28:37 +0600

    Это дз 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
+export function App() {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+      <Header />
+      <input {...register("firstName")} placeholder="First name" />
+      <select {...register("category", { required: true })}>
+        <option value="">Select...</option>
+        <option value="A">Option A</option>
+        <option value="B">Option B</option>
+      </select>
+      <textarea {...register("aboutYou")} placeholder="About you" />
+      <p>{data}</p>
+      <input type="submit" />
+    </form>
   );
 }
 
-export default App;
+// export default App;
